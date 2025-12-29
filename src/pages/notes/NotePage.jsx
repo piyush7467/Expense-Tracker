a162d4268b5745abfae5b43573472a5772ae3bff
Piyush Shakya <piyushshakya112@gmail.com>
2025-12-29 23:34:21 +0530

    improving ui for   page for notes

--- src/pages/notes/NotePage.jsx
+++ src/pages/notes/NotePage.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
-import AddNoteModal from "./AddNoteModal";
+import AddNoteModal from "./AddNoteModel";
+
 
 const NotesPage = () => {
   const [notes, setNotes] = useState([]);
