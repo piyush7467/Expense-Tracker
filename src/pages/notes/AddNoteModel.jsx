7e57ae481b9abb3680388b538663d77ec77eeca0
Piyush Shakya <piyushshakya112@gmail.com>
2025-12-30 23:05:59 +0530

    removeing bug

--- src/pages/notes/AddNoteModel.jsx
+++ src/pages/notes/AddNoteModel.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
-import { toast, ToastContainer } from "react-toastify";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 
 const AddNoteModal = ({ onClose, onSuccess }) => {
